9cc6f352d8ffefd59904828126364348dbf6556f
Joy Lindström <Joy@JAYLJ-MBP.lan>
2018-02-10 14:51:54 +0100

    space

--- controllers/products_controller.php
+++ controllers/products_controller.php
@@ -1,7 +1,7 @@
 <?php
-
 include(CLASS_DIR .'inc.class.products.php');
-$pagecontent = New products;
-include(TEMPLATES_FOLDER . 'products.tpl.php');
 
+$pagecontent = New products;
+
+include(TEMPLATES_FOLDER . 'products.tpl.php');
 ?>
