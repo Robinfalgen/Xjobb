7d9d888ec5d49780822efd77bff7046341d3c0bb
RobinL <robin@nordicwebteam.se>
2018-03-06 13:37:46 +0100

    cart controller inc.class.cart

--- controllers/cart_controller.php
+++ controllers/cart_controller.php
@@ -1,38 +1,9 @@
 <?php
-	require INCLUDES_FOLDER.'cart.inc.php';
-	if(isset($_SERVER['HTTP_REFERER'])) {
-		$referer_url = $_SERVER['HTTP_REFERER'];
-		}else{
-			$referer_url = "";
-		}
 
-	$_SESSION['cart'] === $pid = 0;	
-	if (isset($_GET['action']) && $_GET['action'] == 'addtocart') {
-		$pid = $_POST['pid'];
-		$qty = $_POST['qty'];
-		if (array_key_exists($pid, $_SESSION['cart']) ){
-			$_SESSION['cart'][ $pid ] += $qty;
-			header("location:".$referer_url);
-		}else{
-			$_SESSION['cart'][ $pid ] = $qty;
-			header("location:".$referer_url);
-		}
-	}
-
-	if (isset($_GET['action']) && $_GET['action'] == 'removecartitem' && isset($_GET['pid']) ) {
-		unset($_SESSION['cart'][$_GET['pid']] );
-		header("location:".$referer_url);
-	}
+require INCLUDES_FOLDER.'cart.inc.php';
+include(CLASS_DIR . 'inc.class.cart.php');
 	
-	if ($_GET['action'] == 'updatecart'){
-		foreach ($_POST['cartitems'] as $pid => $qty) {
-			if($qty < 1) {
-				unset( $_SESSION['cart'][$pid] );
-				}else{
-					$_SESSION['cart'][ $pid ] = $qty;
-					header("location:".$referer_url);
-				}
-		}
-	}
+	$currentcart = new Cart;
+	$currentcart->cartController();
 
-var_dump($_POST);
+	
