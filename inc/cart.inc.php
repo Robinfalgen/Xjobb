e8b5ca59879e32ec03cc923804263f5d442fdad0
Joy Lindström <Joy@JAYLJ-MBP.lan>
2018-03-24 16:30:48 +0100

    snygga till i fil

--- inc/cart.inc.php
+++ inc/cart.inc.php
@@ -1,12 +1,10 @@
 <?php
-require_once (INCLUDES_FOLDER.'db.conn.inc.php');
+require_once (INCLUDES_FOLDER.'db.conn.inc.php'); 
 
+$cart = array();
+if(isset($_SESSION['cart'])) {
 
-	$cart = array();
-	if(isset($_SESSION['cart'])) {
-	
 	$idsInCart = array_keys($_SESSION['cart']);
-
 	$idsInCart = implode($idsInCart, ',');
 	$connect = new DBconn;
 	$conn = $connect->conn();
@@ -17,26 +15,22 @@ require_once (INCLUDES_FOLDER.'db.conn.inc.php');
 	$stmt->execute();
 	$result = $stmt->fetchAll();
 
+	$totalSum = 0;
 
-$totalSum = 0;
 
-if(isset($result)) {
-	foreach ($result as $product) {
-		$prod = array();
-		$prod['qty'] = $_SESSION['cart'][$product['product_id']];
-		$prod['img'] = $product['img'];
-		$prod['price'] = $product['product_price'];
-		$prod['name'] = $product['product_title'];
-		$prod['sum'] = $prod['price'] * $prod['qty'];
-		$totalSum += $prod['sum'];
-		
-		$cart['cartItems'][$product['product_id']] = $prod;
-
+	if(isset($result)) {
+		foreach ($result as $product) {
+			$prod = array();
+			$prod['qty'] = $_SESSION['cart'][$product['product_id']];
+			$prod['img'] = $product['img'];
+			$prod['price'] = $product['product_price'];
+			$prod['name'] = $product['product_title'];
+			$prod['sum'] = $prod['price'] * $prod['qty'];
+			$totalSum += $prod['sum'];
+			
+			$cart['cartItems'][$product['product_id']] = $prod;
+		}
 	}
-}
-
-$cart['total'] = $totalSum;
-
-
+	$cart['total'] = $totalSum;
 }
 
